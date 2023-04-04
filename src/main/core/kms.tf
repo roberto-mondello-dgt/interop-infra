951983718c95565fca246ab947f807315ced4805
eugenard-pagopa <104842355+eugenard-pagopa@users.noreply.github.com>
2023-04-04 09:05:23 +0200

    feat: KMS stack (#10)

---
+++ src/main/core/kms.tf
@@ -0,0 +1,27 @@
+resource "aws_kms_key" "interop" {
+    customer_master_key_spec  = "RSA_2048"
+    key_usage                 = "SIGN_VERIFY"
+    policy = jsonencode(
+        {
+        Id        = "DefaultPolicy"
+        Statement = [
+            {
+                Action    = "kms:*"
+                Effect    = "Allow"
+                Principal = {
+                    AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+                }
+                Resource  = "*"
+                Sid       = "EnableIAMPolicies"
+                },
+            ]
+        Version   = "2012-10-17"
+        }
+    )   
+}
+
+
+resource "aws_kms_alias" "interop" {
+  name          = "alias/interop-rsa2048-${var.env}"
+  target_key_id = aws_kms_key.interop.key_id
+}
