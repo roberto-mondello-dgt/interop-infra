fc8d56f7eeed41307d1a3c6177dad3ea32d06ce5
Michele De Simone <michele.desimone@pagopa.it>
2023-08-04 17:59:41 +0200

    feat: adot-collector monitoring

--- src/main/core/k8s-monitoring-deployments.tf
+++ src/main/core/k8s-monitoring-deployments.tf
@@ -18,3 +18,22 @@ module "k8s_deployment_monitoring" {
 
   create_dashboard = true
 }
+
+# TODO: refactor module to better support StatefulSets? the "unavailable-pods" alarm won't work
+module "k8s_adot_monitoring" {
+  source = "./modules/k8s-deployment-monitoring"
+
+  env                 = var.env
+  eks_cluster_name    = module.eks_v2.cluster_name
+  k8s_namespace       = "aws-observability"
+  k8s_deployment_name = "adot-collector"
+  sns_topics_arns     = [aws_sns_topic.platform_alarms.arn]
+
+  create_alarms = true
+
+  avg_cpu_alarm_threshold           = 70
+  avg_memory_alarm_threshold        = 70
+  performance_alarms_period_seconds = 300 # 5 minutes
+
+  create_dashboard = true
+}
