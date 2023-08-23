195258ca567a093f754f6cd5e5fb1ca216846140
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-08-23 11:39:28 +0200

    PIN-3701: Buildo secrets (#43)

---
+++ src/main/core/buildo-secrets.tf
@@ -0,0 +1,23 @@
+resource "aws_secretsmanager_secret" "postgres_db_username_refactor" {
+  count = var.env == "dev" ? 1 : 0
+
+  name = "postgres-db-username-refactor"
+}
+
+resource "aws_secretsmanager_secret" "postgres_db_password_refactor" {
+  count = var.env == "dev" ? 1 : 0
+
+  name = "postgres-db-password-refactor"
+}
+
+resource "aws_secretsmanager_secret" "docdb_username_refactor" {
+  count = var.env == "dev" ? 1 : 0
+
+  name = "documentdb-username-refactor"
+}
+
+resource "aws_secretsmanager_secret" "docdb_password_refactor" {
+  count = var.env == "dev" ? 1 : 0
+
+  name = "documentdb-password-refactor"
+}
