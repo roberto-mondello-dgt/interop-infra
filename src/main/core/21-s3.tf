07cd816aa8ba2b1074d843fd6084fa2bc49d0ce4
Michele De Simone <michele.desimone@pagopa.it>
2023-11-30 15:41:46 +0100

    feat: add data preparation bucket in QA

--- src/main/core/21-s3.tf
+++ src/main/core/21-s3.tf
@@ -585,3 +585,21 @@ module "msk_custom_plugins_bucket" {
     enabled = true
   }
 }
+
+module "data_preparation_bucket" {
+  count = var.env == "qa" ? 1 : 0
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.14.1"
+
+  bucket = format("interop-data-preparation-%s", var.env)
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+}
