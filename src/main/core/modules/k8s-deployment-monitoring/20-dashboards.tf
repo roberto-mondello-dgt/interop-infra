4da81f40ceb4380db9360291ebdf916c5417daf7
Michele De Simone <michele.desimone@pagopa.it>
2024-01-08 14:36:43 +0100

    refactor: use templatefile() for better compatibility

--- src/main/core/modules/k8s-deployment-monitoring/20-dashboards.tf
+++ src/main/core/modules/k8s-deployment-monitoring/20-dashboards.tf
@@ -1,18 +1,14 @@
 # TODO: make time periods configurable
-data "template_file" "monitoring_dashboard" {
-  template = file("${path.module}/k8s_deployment_dashboard.tpl.json")
-
-  vars = {
-    Region                   = data.aws_region.current.name
-    ClusterName              = var.eks_cluster_name
-    Namespace                = var.k8s_namespace
-    Service                  = var.k8s_deployment_name
-    ServiceType              = "deployment"
-    NumberOfDigitsMultiplier = pow(10, var.number_of_digits)
-  }
-}
-
 resource "aws_cloudwatch_dashboard" "k8s_deployment" {
   dashboard_name = format("k8s-%s-%s", var.k8s_deployment_name, var.env)
-  dashboard_body = data.template_file.monitoring_dashboard.rendered
+  dashboard_body = templatefile("${path.module}/k8s_deployment_dashboard.tpl.json",
+    {
+      Region                   = data.aws_region.current.name
+      ClusterName              = var.eks_cluster_name
+      Namespace                = var.k8s_namespace
+      Service                  = var.k8s_deployment_name
+      ServiceType              = "deployment"
+      NumberOfDigitsMultiplier = pow(10, var.number_of_digits)
+    }
+  )
 }
--- src/main/core/modules/queue-monitoring/queue-monitoring.tf
+++ src/main/core/modules/queue-monitoring/queue-monitoring.tf
@@ -6,14 +6,6 @@ data "aws_sqs_queue" "queue" {
   name = var.queue_name
 }
 
-data "template_file" "monitoring_dashboard_templates" {
-  template = file("${path.module}/queue_monitoring_dahsboard.json")
-  vars = {
-    QueueName = var.queue_name,
-    Region    = var.region
-  }
-}
-
 resource "aws_cloudwatch_metric_alarm" "sqs_alarm" {
   alarm_name          = "${var.queue_name}-message-age-${var.env}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -34,5 +26,10 @@ resource "aws_cloudwatch_metric_alarm" "sqs_alarm" {
 
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = "sqs-${replace(var.queue_name, ".", "-")}"
-  dashboard_body = data.template_file.monitoring_dashboard_templates.rendered
+  dashboard_body = templatefile("${path.module}/queue_monitoring_dahsboard.json",
+    {
+      QueueName = var.queue_name,
+      Region    = var.region
+    }
+  )
 }
