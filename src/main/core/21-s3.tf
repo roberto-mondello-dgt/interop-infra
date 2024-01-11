5cf92a44d3f2059ed77753c8a46a60bdad19c976
Michele De Simone <michele.desimone@pagopa.it>
2024-01-11 16:04:26 +0100

    feat: frontend additional assets bucket

--- src/main/core/21-s3.tf
+++ src/main/core/21-s3.tf
@@ -619,3 +619,21 @@ module "data_preparation_bucket" {
     enabled = true
   }
 }
+
+module "frontend_additional_assets_bucket" {
+  count = var.env == "dev" ? 1 : 0
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.14.1"
+
+  bucket = format("interop-frontend-additional-assets-%s", var.env)
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = false
+  }
+}
