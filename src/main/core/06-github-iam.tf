1a5ba4d232b46ff4d07222448910062ba1a7169c
Michele De Simone <michele.desimone@pagopa.it>
2024-02-07 15:43:42 +0100

    feat: be-monorepo IAM role

--- src/main/core/06-github-iam.tf
+++ src/main/core/06-github-iam.tf
@@ -59,6 +59,80 @@ resource "aws_iam_role" "github_ecr" {
   }
 }
 
+data "aws_iam_policy_document" "github_monorepo_assume" {
+  count = local.deploy_be_refactor_infra ? 1 : 0
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
+        "repo:pagopa/interop-be-monorepo:*",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_monorepo" {
+  count = local.deploy_be_refactor_infra ? 1 : 0
+
+  name = format("%s-github-monorepo-%s", var.short_name, var.env)
+
+  assume_role_policy = data.aws_iam_policy_document.github_monorepo_assume[0].json
+
+  inline_policy {
+    name = "GithubEcrPolicy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = "ecr:GetAuthorizationToken"
+          Resource = "*"
+        },
+        {
+          Effect = "Allow"
+          Action = [
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:InitiateLayerUpload",
+            "ecr:PutImage",
+            "ecr:UploadLayerPart"
+          ]
+          Resource = "*"
+        }
+      ]
+    })
+  }
+
+  inline_policy {
+    name = "KubeConfigPolicy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = "eks:DescribeCluster"
+          Resource = module.eks_v2.cluster_arn
+        }
+      ]
+    })
+  }
+}
+
 data "aws_iam_policy_document" "github_assume_ecs" {
   statement {
     effect  = "Allow"
