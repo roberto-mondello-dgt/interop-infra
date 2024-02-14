164fab29723296f7788db528f5096067d2eed94b
Michele De Simone <michele.desimone@pagopa.it>
2024-02-14 10:39:37 +0100

    fix: datalake exporter IRSA

--- src/main/core/eks-sa-roles.tf
+++ src/main/core/eks-sa-roles.tf
@@ -562,6 +562,26 @@ module "be_dtd_metrics_irsa" {
   }
 }
 
+module "be_datalake_data_export_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.20.0"
+
+  role_name = format("interop-be-datalake-data-export-%s", var.env)
+
+  assume_role_condition_test = local.deploy_be_refactor_infra ? "StringLike" : "StringEquals"
+
+  oidc_providers = {
+    cluster = {
+      provider_arn               = module.eks_v2.oidc_provider_arn
+      namespace_service_accounts = ["${local.k8s_namespace_irsa}:interop-be-datalake-data-export"]
+    }
+  }
+
+  role_policy_arns = {
+    be_datalake_data_export = aws_iam_policy.be_datalake_data_export.arn
+  }
+}
+
 module "aws_load_balancer_controller_irsa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
@@ -596,25 +616,5 @@ module "adot_iam_role" {
 
   role_policy_arns = {
     cloudwatch = data.aws_iam_policy.cloudwatch_agent_server.arn
-  }
-}
-
-module "be_datalake_data_export_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.20.0"
-
-  role_name = format("%s-interop-be-datalake-data-export", local.role_prefix)
-
-  assume_role_condition_test = local.deploy_be_refactor_infra ? "StringLike" : "StringEquals"
-
-  oidc_providers = {
-    cluster = {
-      provider_arn               = module.eks_v2.oidc_provider_arn
-      namespace_service_accounts = ["${local.k8s_namespace_irsa}:interop-be-datalake-data-export"]
-    }
-  }
-
-  role_policy_arns = {
-    be_datalake_data_export = aws_iam_policy.be_datalake_data_export.arn
   }
 }
