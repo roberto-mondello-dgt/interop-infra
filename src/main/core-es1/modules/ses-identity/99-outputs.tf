dc1f05ecddeb4e898c48000e4e3ed1f1e1db52b5
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2024-08-08 17:01:29 +0200

    feat: add ses_configuration_set_name output to ses-identity module (#99)

--- src/main/core-es1/modules/ses-identity/99-outputs.tf
+++ src/main/core-es1/modules/ses-identity/99-outputs.tf
@@ -12,3 +12,8 @@ output "ses_configuration_set_arn" {
   description = "ARN of the Configuration Set managed by this module"
   value       = aws_sesv2_configuration_set.this.arn
 }
+
+output "ses_configuration_set_name" {
+  description = "Name of the Configuration Set managed by this module"
+  value       = aws_sesv2_configuration_set.this.configuration_set_name
+}
