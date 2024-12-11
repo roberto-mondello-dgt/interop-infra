b53c34cb53c4133835feeb2b0a8c23d167f428f5
Michele De Simone <michele.desimone@pagopa.it>
2024-12-11 14:42:47 +0100

    fix: ext module Git reference

--- src/main/core-es1/k8s-monitoring-deployments.tf
+++ src/main/core-es1/k8s-monitoring-deployments.tf
@@ -2,7 +2,7 @@
 module "k8s_deployment_monitoring" {
   for_each = toset(concat(var.k8s_monitoring_deployments_names, var.k8s_monitoring_internal_deployments_names))
 
-  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
+  source = "git@github.com:pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
 
   env                 = var.env
   eks_cluster_name    = module.eks.cluster_name
@@ -29,7 +29,7 @@ module "be_refactor_k8s_deployment_monitoring" {
   for_each = toset([for d in concat(var.be_refactor_k8s_monitoring_deployments_names, var.be_refactor_k8s_monitoring_internal_deployments_names)
   : d if local.deploy_be_refactor_infra])
 
-  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
+  source = "git@github.com:pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
 
   env                 = var.env
   eks_cluster_name    = module.eks.cluster_name
@@ -56,7 +56,7 @@ module "be_refactor_k8s_deployment_monitoring" {
 
 # TODO: refactor module to better support StatefulSets? the "unavailable-pods" alarm won't work
 module "k8s_adot_monitoring" {
-  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
+  source = "git@github.com:pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
 
   env                 = var.env
   eks_cluster_name    = module.eks.cluster_name
@@ -80,7 +80,7 @@ module "k8s_adot_monitoring" {
 module "k8s_auth_server_poc_monitoring" {
   count = var.env == "dev" ? 1 : 0
 
-  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
+  source = "git@github.com:pagopa/interop-infra-commons//terraform/modules/k8s-deployment-monitoring?ref=v1.3.5"
 
   env                 = var.env
   eks_cluster_name    = "interop-poc-token-eks-cluster-dev"
