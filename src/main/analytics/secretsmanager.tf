65cc15c5ca77958a91402e6b19939fc3f100a009
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-01-16 14:16:33 +0100

    feat(analytics): add new redshift users (#119)

--- src/main/analytics/secretsmanager.tf
+++ src/main/analytics/secretsmanager.tf
@@ -1,111 +1,42 @@
 locals {
-  readonly_username                         = format("%s-analytics-readonly", local.project)
-  be_analytics_domain_consumer_username     = module.be_analytics_domain_consumer_irsa.iam_role_name
-  be_analytics_jwt_consumer_username        = module.be_analytics_jwt_consumer_irsa.iam_role_name
-  tracing_be_enriched_data_handler_username = "tracing-be-enriched-data-handler-dev-es1"
-}
-
-resource "aws_secretsmanager_secret" "readonly" {
-  name = format("redshift/%s-analytics-%s/users/%s", local.project, var.env, local.readonly_username)
-
-  # Necessary for Redshift log in integration
-  tags = merge(var.tags, {
-    Redshift = ""
-  })
-}
-
-resource "aws_secretsmanager_secret" "be_analytics_domain_consumer" {
-  name = format("redshift/%s-analytics-%s/users/%s", local.project, var.env, local.be_analytics_domain_consumer_username)
-
-  # Necessary for Redshift log in integration
-  tags = merge(var.tags, {
-    Redshift = ""
-  })
-}
-
-resource "aws_secretsmanager_secret" "be_analytics_jwt_consumer" {
-  name = format("redshift/%s-analytics-%s/users/%s", local.project, var.env, local.be_analytics_jwt_consumer_username)
-
-  # Necessary for Redshift log in integration
-  tags = merge(var.tags, {
-    Redshift = ""
-  })
-}
-
-resource "aws_secretsmanager_secret" "tracing_be_enriched_data_handler" {
-  name = format("redshift/%s-analytics-%s/users/%s", local.project, var.env, local.tracing_be_enriched_data_handler_username)
-
-  # Necessary for Redshift log in integration
-  tags = merge(var.tags, {
-    Redshift = ""
-  })
-}
-
-data "aws_secretsmanager_random_password" "readonly" {
-  password_length            = 30
-  exclude_characters         = "\"@/'\\ "
-  require_each_included_type = true
-}
-
-data "aws_secretsmanager_random_password" "be_analytics_domain_consumer" {
-  password_length            = 30
-  exclude_characters         = "\"@/'\\ "
-  require_each_included_type = true
-}
-
-data "aws_secretsmanager_random_password" "be_analytics_jwt_consumer" {
-  password_length            = 30
-  exclude_characters         = "\"@/'\\ "
-  require_each_included_type = true
-}
-
-data "aws_secretsmanager_random_password" "tracing_be_enriched_data_handler" {
-  password_length            = 30
-  exclude_characters         = "\"@/'\\ "
-  require_each_included_type = true
-}
-
-resource "aws_secretsmanager_secret_version" "readonly" {
-  secret_id = aws_secretsmanager_secret.readonly.id
-  secret_string = jsonencode({
-    username = local.readonly_username
-    password = data.aws_secretsmanager_random_password.readonly.random_password
-  })
-
-  lifecycle {
-    ignore_changes = [secret_string]
+  redshift_users = {
+    readonly                         = format("%s-analytics-readonly", local.project)
+    be_analytics_domain_consumer     = module.be_analytics_domain_consumer_irsa.iam_role_name
+    be_analytics_jwt_consumer        = module.be_analytics_jwt_consumer_irsa.iam_role_name
+    tracing_be_enriched_data_handler = "tracing-be-enriched-data-handler-dev-es1"
+    lorenzo_giorgi                   = "lorenzo_giorgi"
+    eduardo_mihalache                = "eduardo_mihalache"
+    diego_longo                      = "diego_longo"
+    roberto_taglioni                 = "roberto_taglioni"
   }
 }
 
-resource "aws_secretsmanager_secret_version" "be_analytics_domain_consumer" {
-  secret_id = aws_secretsmanager_secret.be_analytics_domain_consumer.id
-  secret_string = jsonencode({
-    username = local.be_analytics_domain_consumer_username
-    password = data.aws_secretsmanager_random_password.be_analytics_domain_consumer.random_password
-  })
+resource "aws_secretsmanager_secret" "redshift_users" {
+  for_each = (local.redshift_users)
 
-  lifecycle {
-    ignore_changes = [secret_string]
-  }
+  name = format("redshift/%s-analytics-%s/users/%s", local.project, var.env, each.value)
+
+  # Necessary for Redshift log in integration
+  tags = merge(var.tags, {
+    Redshift = ""
+  })
 }
 
-resource "aws_secretsmanager_secret_version" "be_analytics_jwt_consumer" {
-  secret_id = aws_secretsmanager_secret.be_analytics_jwt_consumer.id
-  secret_string = jsonencode({
-    username = local.be_analytics_jwt_consumer_username
-    password = data.aws_secretsmanager_random_password.be_analytics_jwt_consumer.random_password
-  })
+data "aws_secretsmanager_random_password" "redshift_users" {
+  for_each = (local.redshift_users)
 
-  lifecycle {
-    ignore_changes = [secret_string]
-  }
+  password_length            = 30
+  exclude_characters         = "\"@/'\\ "
+  require_each_included_type = true
 }
 
-resource "aws_secretsmanager_secret_version" "tracing_be_enriched_data_handler" {
-  secret_id = aws_secretsmanager_secret.tracing_be_enriched_data_handler.id
+resource "aws_secretsmanager_secret_version" "redshift_users" {
+  for_each = (local.redshift_users)
+
+  secret_id = aws_secretsmanager_secret.redshift_users[each.key].id
   secret_string = jsonencode({
-    username = local.tracing_be_enriched_data_handler_username
-    password = data.aws_secretsmanager_random_password.tracing_be_enriched_data_handler.random_password
+    username = each.value
+    password = data.aws_secretsmanager_random_password.redshift_users[each.key].random_password
   })
 
   lifecycle {
--- src/main/analytics/sql/dev/01-create-users.sql
+++ src/main/analytics/sql/dev/01-create-users.sql
@@ -2,3 +2,7 @@ CREATE USER "interop-be-analytics-domain-consumer-dev-es1" WITH PASSWORD '';
 CREATE USER "interop-be-analytics-jwt-consumer-dev-es1" WITH PASSWORD '';
 CREATE USER "tracing-be-enriched-data-handler-dev-es1" WITH PASSWORD '';
 CREATE USER "interop-analytics-readonly" WITH PASSWORD '';
+CREATE USER "lorenzo_giorgi" WITH PASSWORD '';
+CREATE USER "eduardo_mihalache" WITH PASSWORD '';
+CREATE USER "diego_longo" WITH PASSWORD '';
+CREATE USER "roberto_taglioni" WITH PASSWORD '';
