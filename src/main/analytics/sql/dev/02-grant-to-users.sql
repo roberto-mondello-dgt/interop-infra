be512ea446318cb84cc14547b9b112bb86bd69e9
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-01-21 09:23:29 +0100

    feat(analytics): add grants to redshift users (#120)

--- src/main/analytics/sql/dev/02-grant-to-users.sql
+++ src/main/analytics/sql/dev/02-grant-to-users.sql
@@ -1,17 +1,21 @@
-GRANT usage ON SCHEMA "interop_dev.domain" TO "interop-be-analytics-domain-consumer-dev-es1";
+GRANT usage ON SCHEMA interop_dev.domain TO "interop-be-analytics-domain-consumer-dev-es1";
 
-GRANT usage ON SCHEMA "interop_dev.jwt" TO "interop-be-analytics-jwt-consumer-dev-es1";
+GRANT usage ON SCHEMA interop_dev.jwt TO "interop-be-analytics-jwt-consumer-dev-es1";
 
-GRANT usage ON SCHEMA "tracing_dev.traces" TO "tracing-be-enriched-data-handler-dev-es1";
+GRANT usage ON SCHEMA tracing_dev.traces TO "tracing-be-enriched-data-handler-dev-es1";
 
-GRANT usage ON SCHEMA "interop_dev.domain" TO "interop-analytics-readonly";
-GRANT SELECT ON ALL TABLES IN SCHEMA "interop_dev.domain" TO "interop-analytics-readonly";
-ALTER DEFAULT PRIVILEGES IN SCHEMA "interop_dev.domain" GRANT SELECT ON TABLES TO "interop-analytics-readonly";
+GRANT usage ON SCHEMA interop_dev.domain TO "interop-analytics-readonly";
+GRANT SELECT ON ALL TABLES IN SCHEMA interop_dev.domain TO "interop-analytics-readonly";
+ALTER DEFAULT PRIVILEGES IN SCHEMA interop_dev.domain GRANT SELECT ON TABLES TO "interop-analytics-readonly";
 
-GRANT usage ON SCHEMA "interop_dev.jwt" TO "interop-analytics-readonly";
-GRANT SELECT ON ALL TABLES IN SCHEMA "interop_dev.jwt" TO "interop-analytics-readonly";
-ALTER DEFAULT PRIVILEGES IN SCHEMA "interop_dev.jwt" GRANT SELECT ON TABLES TO "interop-analytics-readonly";
+GRANT usage ON SCHEMA interop_dev.jwt TO "interop-analytics-readonly";
+GRANT SELECT ON ALL TABLES IN SCHEMA interop_dev.jwt TO "interop-analytics-readonly";
+ALTER DEFAULT PRIVILEGES IN SCHEMA interop_dev.jwt GRANT SELECT ON TABLES TO "interop-analytics-readonly";
 
-GRANT usage ON SCHEMA "tracing_dev.traces" TO "interop-analytics-readonly";
-GRANT SELECT ON ALL TABLES IN SCHEMA "tracing_dev.traces" TO "interop-analytics-readonly";
-ALTER DEFAULT PRIVILEGES IN SCHEMA "tracing_dev.traces" GRANT SELECT ON TABLES TO "interop-analytics-readonly";
+GRANT usage ON SCHEMA tracing_dev.traces TO "interop-analytics-readonly";
+GRANT SELECT ON ALL TABLES IN SCHEMA tracing_dev.traces TO "interop-analytics-readonly";
+ALTER DEFAULT PRIVILEGES IN SCHEMA tracing_dev.traces GRANT SELECT ON TABLES TO "interop-analytics-readonly";
+
+GRANT create ON DATABASE interop_dev TO "lorenzo_giorgi", "eduardo_mihalache", "diego_longo", "roberto_taglioni";
+
+GRANT create, usage ON SCHEMA interop_dev.domain, interop_dev.jwt TO "lorenzo_giorgi", "eduardo_mihalache", "diego_longo", "roberto_taglioni";
