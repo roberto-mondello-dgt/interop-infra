ee369fc72522518a9b24be68cc94831d7bf28f4a
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-02-21 18:32:09 +0100

    fix(analytics): edit jwt-audit sqs queue name (#129)

--- src/main/analytics/40-sqs.tf
+++ src/main/analytics/40-sqs.tf
@@ -1,7 +1,7 @@
 resource "aws_sqs_queue" "jwt_audit" {
   count = local.deploy_data_ingestion_resources ? 1 : 0
 
-  name = format("%s-jwt-audit-analytics-%s", local.project, var.env)
+  name = format("%s-analytics-jwt-audit-%s", local.project, var.env)
 
   message_retention_seconds = 1209600 # 14 days
   max_message_size          = 262144  # 256 KB
