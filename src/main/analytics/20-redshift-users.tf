002d31f282f981bbb368d7469c709dc2e8dbcbae
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-03-10 11:59:26 +0100

    feat(analytics): managing redshift users using postgresql-user module (#133)

---
+++ src/main/analytics/20-redshift-users.tf
@@ -0,0 +1,75 @@
+locals {
+  eks_secret_default_tags = {
+    EKSClusterName                     = data.aws_eks_cluster.core.name
+    EKSClusterNamespacesSpaceSeparated = join(" ", [var.analytics_k8s_namespace])
+    TerraformState                     = local.terraform_state
+  }
+}
+
+module "redshift_flyway_pgsql_user" {
+  count = local.deploy_data_ingestion_resources ? 1 : 0
+
+  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/postgresql-user?ref=v1.6.1"
+
+  username = "interop_analytics_flyway_user"
+
+  generated_password_length = 30
+  secret_prefix             = format("redshift/%s/users/", aws_redshift_cluster.analytics[0].cluster_identifier)
+
+  secret_tags = merge(local.eks_secret_default_tags,
+    {
+      EKSReplicaSecretName = "redshift-flyway-user"
+    }
+  )
+
+  db_host = aws_redshift_cluster.analytics[0].endpoint
+  db_port = aws_redshift_cluster.analytics[0].port
+  db_name = aws_redshift_cluster.analytics[0].database_name
+
+  db_admin_credentials_secret_arn = aws_secretsmanager_secret.redshift_master[0].arn
+
+  additional_sql_statements = <<-EOT
+    GRANT CREATE ON DATABASE "${aws_redshift_cluster.analytics[0].database_name}" TO interop_analytics_flyway_user
+  EOT
+}
+
+locals {
+  be_app_psql_usernames = local.deploy_data_ingestion_resources ? {
+    jwt_audit_analytics_writer_user = {
+      sql_name        = "interop_be_jwt_audit_analytics_writer_${var.env}",
+      k8s_secret_name = "redshift-jwt-audit-analytics-writer-user"
+    },
+    domains_analytics_writer_user = {
+      sql_name        = "interop_be_domains_analytics_writer_${var.env}",
+      k8s_secret_name = "redshift-domains-analytics-writer-user"
+    },
+    alb_logs_analytics_writer_user = {
+      sql_name        = "interop_be_alb_logs_analytics_writer_${var.env}",
+      k8s_secret_name = "redshift-alb-logs-analytics-writer-user"
+    }
+  } : {}
+}
+
+# PostgreSQL users with no initial grants. The grants will be applied by Flyway
+module "redshift_be_app_pgsql_user" {
+  source = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/postgresql-user?ref=v1.6.1"
+
+  for_each = local.be_app_psql_usernames
+
+  username = each.value.sql_name
+
+  generated_password_length = 30
+  secret_prefix             = format("redshift/%s/users/", aws_redshift_cluster.analytics[0].cluster_identifier)
+
+  secret_tags = merge(local.eks_secret_default_tags,
+    {
+      EKSReplicaSecretName = each.value.k8s_secret_name
+    }
+  )
+
+  db_host = aws_redshift_cluster.analytics[0].endpoint
+  db_port = aws_redshift_cluster.analytics[0].port
+  db_name = aws_redshift_cluster.analytics[0].database_name
+
+  db_admin_credentials_secret_arn = aws_secretsmanager_secret.redshift_master[0].arn
+}
