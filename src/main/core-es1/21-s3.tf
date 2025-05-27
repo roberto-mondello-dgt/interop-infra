8fe74aebbb0f99b0d18a7c380bf9903e9de504ee
Michele De Simone <michele.desimone@pagopa.it>
2025-05-27 17:42:55 +0200

    feat: S3 for APIGW OpenAPIs

--- src/main/core-es1/21-s3.tf
+++ src/main/core-es1/21-s3.tf
@@ -658,3 +658,19 @@ module "datalake_interface_export_bucket" {
     enabled = true
   }
 }
+
+module "apigw_openapi_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = format("%s-apigw-openapi-%s-es1", local.project, var.env)
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+}
