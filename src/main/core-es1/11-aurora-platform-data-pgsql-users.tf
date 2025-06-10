7743912756bab906db7f25fec89d7bb9155744b6
Michele De Simone <michele.desimone@pagopa.it>
2025-06-10 14:30:49 +0200

    refactor: PGSQL users list

--- src/main/core-es1/11-aurora-platform-data-pgsql-users.tf
+++ src/main/core-es1/11-aurora-platform-data-pgsql-users.tf
@@ -65,121 +65,59 @@ module "platform_data_readonly_pgsql_user" {
 }
 
 locals {
-  be_app_psql_usernames = local.deploy_read_model_refactor ? {
-    attribute_registry_rmw_user = {
-      sql_name        = "${var.env}_attribute_registry_rmw_user",
-      k8s_secret_name = "platform-data-attribute-registry-rmw-user"
-    },
-    attribute_registry_process_user = {
-      sql_name        = "${var.env}_attribute_registry_process_user",
-      k8s_secret_name = "platform-data-attribute-registry-process-user"
-    },
-    agreement_rmw_user = {
-      sql_name        = "${var.env}_agreement_rmw_user",
-      k8s_secret_name = "platform-data-agreement-rmw-user"
-    },
-    agreement_process_user = {
-      sql_name        = "${var.env}_agreement_process_user",
-      k8s_secret_name = "platform-data-agreement-process-user"
-    },
-    api_gateway_user = {
-      sql_name        = "${var.env}_api_gateway_user",
-      k8s_secret_name = "platform-data-api-gateway-user"
-    },
-    key_rmw_user = {
-      sql_name        = "${var.env}_key_rmw_user",
-      k8s_secret_name = "platform-data-key-rmw-user"
-    },
-    producer_key_rmw_user = {
-      sql_name        = "${var.env}_producer_key_rmw_user",
-      k8s_secret_name = "platform-data-producer-key-rmw-user"
-    },
-    purpose_rmw_user = {
-      sql_name        = "${var.env}_purpose_rmw_user",
-      k8s_secret_name = "platform-data-purpose-rmw-user"
-    },
-    purpose_process_user = {
-      sql_name        = "${var.env}_purpose_process_user",
-      k8s_secret_name = "platform-data-purpose-process-user"
-    },
-    client_rmw_user = {
-      sql_name        = "${var.env}_client_rmw_user",
-      k8s_secret_name = "platform-data-client-rmw-user"
-    },
-    authorization_process_user = {
-      sql_name        = "${var.env}_authorization_process_user",
-      k8s_secret_name = "platform-data-authorization-process-user"
-    },
-    producer_keychain_rmw_user = {
-      sql_name        = "${var.env}_producer_keychain_rmw_user",
-      k8s_secret_name = "platform-data-producer-keychain-rmw-user"
-    },
-    tenant_rmw_user = {
-      sql_name        = "${var.env}_tenant_rmw_user",
-      k8s_secret_name = "platform-data-tenant-rmw-user"
-    },
-    tenant_process_user = {
-      sql_name        = "${var.env}_tenant_process_user",
-      k8s_secret_name = "platform-data-tenant-process-user"
-    },
-    tenant_rmw_user = {
-      sql_name        = "${var.env}_tenant_rmw_user",
-      k8s_secret_name = "platform-data-tenant-rmw-user"
-    },
-    tenant_process_user = {
-      sql_name        = "${var.env}_tenant_process_user",
-      k8s_secret_name = "platform-data-tenant-process-user"
-    },
-    delegation_rmw_user = {
-      sql_name        = "${var.env}_delegation_rmw_user",
-      k8s_secret_name = "platform-data-delegation-rmw-user"
-    },
-    delegation_process_user = {
-      sql_name        = "${var.env}_delegation_process_user",
-      k8s_secret_name = "platform-data-delegation-process-user"
-    },
-    delegation_items_archiver_user = {
-      sql_name        = "${var.env}_delegation_items_archiver_user",
-      k8s_secret_name = "platform-data-delegation-items-archiver-user"
-    },
-    catalog_rmw_user = {
-      sql_name        = "${var.env}_catalog_rmw_user",
-      k8s_secret_name = "platform-data-catalog-rmw-user"
-    },
-    catalog_process_user = {
-      sql_name        = "${var.env}_catalog_process_user",
-      k8s_secret_name = "platform-data-catalog-process-user"
-    },
-    eservice_template_rmw_user = {
-      sql_name        = "${var.env}_eservice_template_rmw_user",
-      k8s_secret_name = "platform-data-eservice-template-rmw-user"
-    },
-    eservice_template_process_user = {
-      sql_name        = "${var.env}_eservice_template_process_user",
-      k8s_secret_name = "platform-data-eservice-template-process-user"
-    },
-    selfcare_client_users_updater_user = {
-      sql_name        = "${var.env}_selfcare_client_users_updater_user",
-      k8s_secret_name = "platform-data-selfcare-client-users-updater-user"
-    }
-  } : {}
+  be_app_psql_usernames = local.deploy_read_model_refactor ? [
+    "agreement_process_user",
+    "agreement_rmw_user",
+    "anac_certified_attributes_importer_user",
+    "api_gateway_user",
+    "attribute_registry_process_user",
+    "attribute_registry_rmw_user",
+    "authorization_process_user",
+    "catalog_process_user",
+    "catalog_rmw_user",
+    "certified_email_sender_user",
+    "client_rmw_user",
+    "datalake_data_export_user",
+    "delegation_items_archiver_user",
+    "delegation_process_user",
+    "delegation_rmw_user",
+    "dtd_catalog_exporter_user",
+    "eservice_descriptors_archiver_user",
+    "eservice_template_instances_updater_user",
+    "eservice_template_process_user",
+    "eservice_template_rmw_user",
+    "ipa_certified_attributes_importer_user",
+    "ivass_certified_attributes_importer_user",
+    "key_rmw_user",
+    "notification_email_sender_user",
+    "pn_consumers_user",
+    "producer_key_rmw_user",
+    "producer_keychain_rmw_user",
+    "purpose_process_user",
+    "purpose_rmw_user",
+    "selfcare_client_users_updater_user",
+    "tenant_process_user",
+    "tenant_rmw_user",
+    "token_generation_readmodel_checker_user"
+  ] : []
 }
+
 
 # PostgreSQL users with no initial grants. The grants will be applied by Flyway
 module "platform_data_be_app_pgsql_user" {
   source     = "git::https://github.com/pagopa/interop-infra-commons//terraform/modules/postgresql-user?ref=v1.7.1"
   depends_on = [module.platform_data_flyway_pgsql_user]
 
-  for_each = local.be_app_psql_usernames
+  for_each = toset(local.be_app_psql_usernames)
 
-  username = each.value.sql_name
+  username = format("%s_%s", var.env, each.value)
 
   generated_password_length = 30
   secret_prefix             = format("rds/%s/users/", module.platform_data.cluster_id)
 
   secret_tags = merge(local.eks_secret_default_tags,
     {
-      EKSReplicaSecretName = each.value.k8s_secret_name
+      EKSReplicaSecretName = format("platform-data-%s", replace(each.value, "_", "-"))
     }
   )
 
